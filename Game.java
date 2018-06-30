fe52e74ff5489d1339431d0aa865c6ecd29d7f5f
christopher-wolff23 <cwolff12@umd.edu>
2018-06-30 17:18:28 -0400

    Trying a new method for playing sounds.

--- Game.java
+++ Game.java
@@ -138,20 +138,23 @@ public class Game extends JFrame implements Runnable {
 	}
 
 	
-	//for playing sound files
-	public void playSound(String sound){
-		try {
-		   /*Not included with open JDK
-		  Media hit = new Media(new File(bip).toURI().toString());
-		   MediaPlayer player = new MediaPlayer(hit);
-	           mediaPlayer.play();*/
-		//if the audio clips compete for resources
-		}catch(Exception ex){
-			System.out.println("general audio problem");
-			ex.printStackTrace();
-		}
-	}
-
+	public static synchronized void playSound(final String url) {
+  	new Thread(new Runnable() {
+  // The wrapper thread is unnecessary, unless it blocks on the
+  // Clip finishing; see comments.
+    public void run() {
+      try {
+        Clip clip = AudioSystem.getClip();
+        AudioInputStream inputStream = AudioSystem.getAudioInputStream(
+          Main.class.getResourceAsStream("/path/to/sounds/" + url));
+        clip.open(inputStream);
+        clip.start(); 
+      } catch (Exception e) {
+        System.err.println(e.getMessage());
+      }
+    }
+  }).start();
+}
 	//main game loop
 	//this is run when the Thread.start() is run
 	public void run(){
